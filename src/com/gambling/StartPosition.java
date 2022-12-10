e6b7845bcb22d7f44aadf14e08a7ca3901791f19
jayashri23 <jsjankar23@gmail.com>
2022-12-10 23:33:19 +0530

    Gambler start Position

---
+++ src/com/gambling/StartPosition.java
@@ -0,0 +1,9 @@
+package com.gambling;
+
+public class StartPosition {
+    public static void main(String[] args) {
+        int stake =100;
+        int bet =1;
+        System.out.println("Gambler starting Stake Position = " +stake+ "$ and bet = " +bet + "$");
+    }
+}
